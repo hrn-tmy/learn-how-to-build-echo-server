62266b568a5361fd3cf1be0347ca3d85c9b4ebf2
hrn-tmy <mj0829mj0625@icloud.com>
2025-10-23 00:40:51 +0900

    add: DB接続処理実装

--- main.go
+++ main.go
@@ -1,18 +1,55 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-// 必要なこと
-// DB接続
-// Repository層の作成
-// Usecase層の作成
-// Handler層の作成
-// Router層の作成
-// 今回はTODOリストを作成する前提でいいかも
+type post struct {
+  postID int `gorm:"primaryKey"`
+  title string
+  body string
+  createdAt time.Time
+  updateAt time.Time
+  deletedAt gorm.DeletedAt
+}
+
+type dbConfig struct {
+  user string
+  password string
+  host string
+  port string
+  name string
+}
+
+func newDB() (*gorm.DB, error) {
+  if err := godotenv.Load(); err != nil {
+    log.Fatal(err)
+  }
+  cfg := dbConfig{
+    user: os.Getenv("DB_USER"),
+    password: os.Getenv("DB_PASSWORD"),
+    host: os.Getenv("DB_HOST"),
+    port: os.Getenv("DB_PORT"),
+    name: os.Getenv("DB_NAME"),
+  }
+  dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", cfg.user, cfg.password, cfg.host, cfg.port, cfg.name)
+  db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+  if err != nil {
+    return nil, err
+  }
+
+  return db, err
+}
+
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
